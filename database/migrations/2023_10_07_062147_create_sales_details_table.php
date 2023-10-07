c6c7e0f654603cda61dd67609b39adf21175e729
Yusril Anwar <yusril886@gmail.com>
2023-10-07 14:28:53 +0800

    create migration sales and sales details:

---
+++ database/migrations/2023_10_07_062147_create_sales_details_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('sales_id');
+            $table->unsignedBigInteger('inventory_id');
+            $table->integer('qty');
+            $table->integer('price');
+            $table->timestamps();
+            $table->foreign('sales_id')->references('id')->on('sales');
+            $table->foreign('inventory_id')->references('id')->on('inventories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales_details');
+    }
+};
